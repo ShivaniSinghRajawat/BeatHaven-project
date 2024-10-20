9453ed115ca61583bda1dcc28ae81bb7c0032912
Seya <160137292+ShivaniSinghRajawat@users.noreply.github.com>
2024-10-20 19:39:16 +0000

    added profile page

---
+++ static/p_script.js
@@ -0,0 +1,7 @@
+document.querySelector(".edit-btn").addEventListener("click", function() {
+    alert("Edit Profile feature coming soon!");
+});
+
+document.querySelector(".logout-btn").addEventListener("click", function() {
+    alert("Logged out successfully!");
+});
